4f346341d56184e40578138b84dc381bdbaa0612
Cintia Marques <cintiamarques.mk@gmail.com>
2024-04-07 19:45:28 -0300

    Req 11

--- src/main/java/com/betrybe/minhaconta/business/EnergyAccount.java
+++ src/main/java/com/betrybe/minhaconta/business/EnergyAccount.java
@@ -1,7 +1,11 @@
 package com.betrybe.minhaconta.business;
 
+import com.ions.lightdealer.sdk.model.Address;
 import com.ions.lightdealer.sdk.model.Client;
 import com.ions.lightdealer.sdk.model.ElectronicDevice;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -19,6 +23,19 @@ public class EnergyAccount {
    * Req. 11 – Find high consumption device per address.
    */
   public ElectronicDevice[] findHighConsumptionDevices() {
-    return new ElectronicDevice[0];
+    List<ElectronicDevice> highConsumptionDevices = new ArrayList<>();
+    for (Address address : client.getAddressesAsArray()) {
+      ElectronicDevice[] addressDevices = address.getDevicesAsArray();
+      ElectronicDevice highConsumptionDevice = null;
+      double highConsumption = 0;
+      for (ElectronicDevice device : addressDevices) {
+        if (device.monthlyKwh() > highConsumption) {
+            highConsumption = device.monthlyKwh();
+            highConsumptionDevice = device;
+        }
+      }
+        highConsumptionDevices.add(highConsumptionDevice);
+    }
+    return highConsumptionDevices.toArray(new ElectronicDevice[0]);
   }
 }
